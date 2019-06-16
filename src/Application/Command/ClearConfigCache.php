ca04df5af4b9d5995788409708c27df57c23c0a3
kpicaza <kpicaza.sound@gmail.com>
2019-06-16 12:17:19 +0200

    Add set development mode command
    * Reneme config clear cache class
    * Add License file

---
+++ src/Application/Command/ClearConfigCache.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Antidot\DevTools\Application\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ClearConfigCache extends Command
+{
+    public const NAME = 'config:clear-cache';
+    private $config;
+
+    public function __construct(array $config)
+    {
+        parent::__construct();
+        $this->config = $config;
+    }
+
+    protected function configure(): void
+    {
+        $this->setName(self::NAME)
+            ->setDescription('Clear config cache files.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        foreach (['config_cache_path', 'cli_config_cache_path'] as $key) {
+            if (! isset($this->config[$key])) {
+                $output->writeln("<comment>No configuration cache path found</comment>");
+                continue;
+            }
+
+            if (! file_exists($this->config[$key])) {
+                $output->writeln(sprintf(
+                    "<error>Configured config cache file '%s' not found</error>",
+                    $this->config[$key]
+                ));
+                continue;
+            }
+
+            if (false === unlink($this->config[$key])) {
+                $output->writeln(sprintf(
+                    "<error>Error removing config cache file '%s'</error>",
+                    $this->config[$key]
+                ));
+                continue;
+            }
+
+            $output->writeln(sprintf(
+                "<info>Removed configured config cache file '%s'</info>",
+                $this->config[$key]
+            ));
+        }
+    }
+}
--- src/Container/Config/ConfigProvider.php
+++ src/Container/Config/ConfigProvider.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Antidot\DevTools\Container\Config;
 
+use Antidot\DevTools\Application\Command\ClearConfigCache;
 use Antidot\DevTools\Application\Command\SetDevelopmentMode;
 use Antidot\DevTools\Application\Command\ShowContainer;
+use Antidot\DevTools\Container\ClearConfigCacheCommandFactory;
 use Antidot\DevTools\Container\SetDevelopmentModeCommandFactory;
 use Antidot\DevTools\Container\ShowContainerCommandFactory;
 
@@ -16,16 +18,19 @@ class ConfigProvider
         return [
             'console' => [
                 'commands' => [
+                    ClearConfigCache::NAME => ClearConfigCache::class,
                     ShowContainer::NAME => ShowContainer::class,
                     SetDevelopmentMode::NAME => SetDevelopmentMode::class,
                 ],
                 'dependencies' => [
                     'factories' => [
+                        ClearConfigCache::class => ClearConfigCacheCommandFactory::class,
                         ShowContainer::class => ShowContainerCommandFactory::class,
                         SetDevelopmentMode::class => SetDevelopmentModeCommandFactory::class,
                     ],
                 ],
             ],
+            'config_dir' => 'config/services'
         ];
     }
 }
