e9c0586f369be0ccf12e3f3d1a091bae358d9704
kpicaza <kpicaza.sound@gmail.com>
2019-06-22 11:00:28 +0200

    issue/#3: Add descriptions to maker commands

--- src/Application/Command/MakeFactory.php
+++ src/Application/Command/MakeFactory.php
@@ -7,6 +7,7 @@ namespace Antidot\DevTools\Application\Command;
 class MakeFactory extends AbstractMakerCommand
 {
     public const NAME = 'make:factory';
+    protected const COMMAND_DESCRIPTION = 'Creates a factory class for PSR-11 container.';
     protected const FQCN_ARGUMENT_DESCRIPTION = 'Add Full qualified class name for Factory.';
     protected const QUESTION =
         '<fg=blue>Please enter the name of the Factory class <info>[App\Container\MyFactory]</info>: </> ';
